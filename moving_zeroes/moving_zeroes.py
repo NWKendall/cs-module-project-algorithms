75ddd4e676afbe2f5fa83f8908d215f200898f0b
nicbongo <nicbongo@protonmail.com>
2020-05-20 19:52:30 -0300

    moving_zeroes: tests passing

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,14 +2,24 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    i = 0
+    for j in range(len(arr)):
+        if arr[j] is not 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    return arr
+
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+    arr=[0, 3, 1, 0, -2]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+# ALWAYS SWAPS THE FIRST ZERO WITH THE LAST NON ZERO
+# in place solution
+# two pointers i and j
+# i only increases if swapping with j AND when j is not a zero
