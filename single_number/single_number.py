dd8e8dcd0f405aec61ffda6178ae575698fc1804
nicbongo <nicbongo@protonmail.com>
2020-05-20 17:47:21 -0300

    single number passing tests

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,10 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # Your code here    
 
-    pass
+    return 2 * sum(set(arr)) - sum(arr) 
+
 
 
 if __name__ == '__main__':
@@ -13,3 +14,11 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+
+# https://www.geeksforgeeks.org/find-element-appears-array-every-element-appears-twice/
+
+
+# set = will filter out duplicates, only have unique values
+# multiplying this by 2 will double the value, as if there were duplicates
+# subtract the total of the original array
+# the difference will be unique number in arr
