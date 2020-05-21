5b79651239e9d27a0c2ab8036e3dde28c49b9495
nicbongo <nicbongo@protonmail.com>
2020-05-21 12:53:10 -0300

    cookie: small n tests passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,18 +2,24 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+
+def eating_cookies(n, cache=None):
     # Your code here
+    if n <= 0:
+        return 1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
     
-    if n == 0:
-        return 0
-    
-    return eating_cookies(n+1)
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 10
+    num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
 
@@ -25,3 +31,14 @@ if __name__ == "__main__":
 # if n > 0, eat 1
 # subtract eated cookies from n, and recurse
 # counter for every time n = 0
+
+# append each eating method into ways
+# check sub arrays are unique (no duplicates)
+# return len of arrays
+
+
+# ways[0] = [1,1,1,1,1] == 5
+# ways[1] = [2,1,1,1] == 5
+# ways = [[1,1,1,1,1], [2,1,1,1]...etc]
+# check for uniqueness of subarays (no dupes)
+# return len(ways)
